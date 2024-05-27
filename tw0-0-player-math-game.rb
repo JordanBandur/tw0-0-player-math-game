a2774f23057248ece422fc82d5bd78bceea96a8e
Forge <jordanbandur@hotmail.ca>
2024-05-27 08:58:18 -0700

    Add comments

--- tw0-0-player-math-game.rb
+++ tw0-0-player-math-game.rb
@@ -1,5 +1,5 @@
-# Player - represent each player. Stores player state such as their name, and number of
-# lives.
+# Player - represent each player. Stores player state such as their name, number of
+# lives, and scores.
 class Player 
   def initalize(name, score, lives = 3)
     @name = name
@@ -22,13 +22,15 @@ class Player
   end
 end
 
+# The Question class is responsible for generating new math problems for the players to solve. 
+# It stores the two numbers involved in the problem and can calculate the correct answer.
 class Question
   def initalize(num1, num2)
     generate_num
   end
 
-  def generate_num
-    @num1 = rand(1..20)
+  def generate_num # Generates random numbers between 1 and 20
+    @num1 = rand(1..20) 
     @num2 = rand(1..20)
   end
 
@@ -37,6 +39,9 @@ class Question
   end
 end
 
+# The Game class manages the overall flow of the game, including keeping track of turns,
+# checking answers, and determining when the game ends. It switches between players, 
+# checks their answers, and announces the winner when the game ends.
 class Game 
   def initalize(player1, player2, current_player)
     @player1 = Player.new("Player1")
@@ -50,7 +55,7 @@ class Game
     while @player1.is_alive && @player2.is_alive
       Question = Question.new
       puts "#{@correct_answer.name}, What is #{question.num1} + #{question.num2}?"
-      answer = gets.chomp.to_i
+      answer = gets.chomp.to_i # Gets the user input and converts it to an number
 
       if check_answer(answer, question.correct_answer)
         puts "Correct!"
